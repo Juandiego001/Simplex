fa371bd6ac24ed0201c09b5f18871025f7c5a0e0
Usuario ILINF <admin-ilinf@uao.edu.co>
2022-04-06 16:54:00 -0500

    Revert "Quedamos en el calculo de la matriz identidad"
    This reverts commit ee5208e7f91824554275a662246ca63955ce7cf5.
    
    Revert

--- js/index.js
+++ js/index.js
@@ -287,102 +287,11 @@ function contBtnRealizar() {
   });
 }
 
-// Función para realizar multiplicaciones de matrices
 function mmult(matriz1, matriz2) {
 
 }
 
-// Función para calcular la inversa
 function minversa(matriz) {
-  // B =  | 2 3 4 |
-  //      | 2 3 5 |
-  //      | 2 3 6 |
-  // Posiciones = 0 1 2  
-
-  // Se añadirá a la matriz a la que se le calculará la inversa
-  // la matriz identidad y se creara una matriz n x n + cantRs (n x n + cantidad de restricciones)
-  let contador = 0;
-
-  for (let i = 0; i < cantRs; i++) {
-    for (let j = 0; j < cantRs; j++) {
-      if (j == contador) {
-        matriz[[i](cantRs + j)] = 1;
-      } else {
-        matriz[[i](cantRs + j)] = 0;
-      }
-    }
-
-    contador++;
-  }
-
-  // B = | 2 3 4 1 0 0 |
-  //     | 2 3 5 0 1 0 |
-  //     | 2 3 6 0 0 1 |
-  // La variable finish determinará si ya se ha calculado
-  // la identidad de una matriz
-  let finish = false;
-
-  while(!finish) {
-    
-  }
-}
-
-// Tercera función principal.
-function simplexRecursivo(C, CB, XB, A, B, b, Bi, CBBi, BiA, CBBiAmC, CBBib, Bib, iteraciones) {
-  // Si la cantidad de iteraciones es 0,
-  // es porque se hará la iteración inicial
-  // Por ende, nada más se debe decidir la variable que sale
-  // y la variable que entra.
-  // Para ello se escoge la variable más negativa.
-  if (iteraciones.length == 0) {
-    let aux = 0;
-    for (let i = 0; i < cantVd; i++) {
-      let inicial = CBBiAmC[i];  
-      if (inicial < aux) {
-        aux = inicial;
-      }
-    }
-
-    // pos1 determinará la posición en la que se encontró el valor menor
-    // así se obtendrá la variable de decisión que ingresa a la función
-    let pos1 = CBBiAmC.indexOf(aux);
-
-    // Se crea un arreglo donde se irán guardando todos los ratios
-    let ratios = [];
-
-    // Posteriormente, se realiza un ciclo para ir almacenando cada ratio
-    for (let j = 0; j < cantRs; j++) {
-      let ratio = BiA[j][pos1]/Bib[j];
-      console.log("Ratio: " + ratio);
-      ratios[j] = cociente;
-    }
-
-    // Se crea una variable auxRatio para almacenar el mayor valor del
-    // arreglo de ratios
-    let auxRatio = Number.MAX_VALUE;
-    for (let j = 0; j < cantRs; j++) {
-      let inicial = ratios[j];
-      if (inicial < auxRatio && inicial > 0) {
-        auxRatio = inicial;
-      }
-    }
-
-    // La pos2 se obtiene a partir de los ratios
-    // y permite obtener la variable de salida
-    let pos2 = ratios.indexOf(auxRatio);
-
-    // Se cambia CB
-    CB[pos2] = C[pos2];
-
-    // Se cambia la matriz B
-    for (let j = 0; j < cantRs; j++) {
-      B[j][pos2] = A[j][pos2];
-    }
-
-    Bi = minversa(B);
-
-
-  }
 
 }
 
@@ -397,58 +306,24 @@ function realizarSimplex() {
   let C = [];
 
   // Coeficientes en la función objetivo
-  let CB = [];
+  let CB = [0,0,0];
 
   // Variables de holgura
-  let XB = [];
-
-  // Matriz A
-  // Matriz de coeficientes de restricciones en la función objetivo
+  let XB = ['s1', 's2', 's3'];
   let A = [];
-
-  // Matriz B
-  // Matriz de coeficientes en las restricciones que entran a la función objetivo
   let B = [];
-
-  // Vector b
-  // Vector de resultados de restricciones
   let b = [];
-
-  // CB x Inversa de la matriz B x A - C
   let CBBiAmC = [];
-
-  // CB x Inversa de la matriz B
   let CBBi = [];
-
-  // CB x Inversa de la matriz B x Vector B
   let CBBib = 0;
-
-  // Inversa de la matriz B x A
   let BiA = [];
-
-  // Inversa de la matriz B
   let Bi = [];
-
-  // Inversa de la matriz B x Vector b
   let Bib = [];
 
   // Vector C
   // Coeficientes de las variables básicas en la función objetivo
   for (let i  = 0; i < cantVd; i++) {
     C[i] = parseInt(document.getElementById(`inputVd${(i+1)}`).value);
-  }
-
-  // Vector CB
-  // Coeficientes de las variables básicas (Variables que están en la función objetivo)
-  // Inicialmente en 0
-  for (let i = 0; i < cantRs; i++) {
-    CB[i] = 0;
-  }
-
-  // Vector XB
-  // Variables que entran en la iteración
-  for (let i = 0; i < cantRs; i++) {
-    XB[i] = `S${(i+1)}`;
   }
 
   // Matriz A
@@ -460,24 +335,6 @@ function realizarSimplex() {
     }
   }
 
-  // Matriz B
-  // Matriz de evaluación de coeficientes de las restricciones que entran
-  // Primero es la matriz identidad
-  let contador = 0; // Me ayuda a crear la matriz identidad
-  for (let i = 0; i < cantRs; i++) {
-    B[i] = new Array(cantRs);
-    for (let j = 0; j < cantRs; j++) {
-      if (j == contador) {
-        B[i][j] = 1;
-      } else {
-        B[i][j] = 0;
-      }
-    }
-    contador++;
-  }
-
-  console.log(B);
-
   // Vector b
   // Valores de cada una de las restricciones
   for (let i = 0; i < cantRs; i++) {
@@ -485,34 +342,7 @@ function realizarSimplex() {
     b[i] = menorIgual;
   }
 
-  // Inicialmente Bi es igual a B
-  Bi = B;
-
-  // CB x Inversa de la matriz B inicialmente es 0
-  for (let i = 0; i < cantRs; i++) {
-    CB[i] = 0;
-  }
   
-  // Inversa de la matriz B x A inicialmente es igual a A
-  BiA = A;
-
-  // CB x Inversa de la matriz B x A - C inicialmente es igual a C, solo que invertidos
-  for (let i = 0; i < cantVd; i++) {
-    CBBiAmC[i] = (C[i] * -1)
-  }
-
-  // Inicialmente CB x Inversa de la matriz B x Vector b es igual a 0;
-  CBBib = 0;
-
-  // Inicialmente Inversa de la matriz B x Vector b es igual al Vector b
-  Bib = b;
-
-  // Inicialmente, la cantidad de iteraciones tendrá un cantidad de iteraciones igual a 0
-  iteraciones = [] 
-
-  iteraciones = simplexRecursivo(C, CB, XB, A, B, b, Bi, CBBi, BiA, CBBiAmC, CBBib, Bib, iteraciones);
-
-
 
 
   
